a59287b8532ad3aaf8c024dc52d21a11ac91c91f
fabientolsy <2031900@cgmatane.qc.ca>
2023-03-23 11:12:31 -0400

    Update detectionContact.cs

--- Assets/detectionContact.cs
+++ Assets/detectionContact.cs
@@ -12,14 +12,13 @@ public class detectionContact : MonoBehaviour
     private void Start()
     {
         cube = transform.parent.gameObject.GetComponent<gestionCube>();
-        temps = 0f;
         
     }
     private void OnTriggerEnter(Collider collision)
     {        
-        if (collision.name == "hands:b_l_index3" && Time.time - temps >= 2.0f)
+        if (collision.name == "hands:b_l_index3" && Time.time - temps >= 5f)
         {
-            Debug.Log("Temps depuis le debut: " + temps );
+            Debug.LogWarning("Temps depuis le debut: " + temps );
             Debug.Log(collision.name + " est entre en collision avec " + this.gameObject.name);
 
             cube.afficher(this.gameObject.name);     
