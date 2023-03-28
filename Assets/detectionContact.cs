0c73b6b35f1e52b868148420b2a4f75ab7e0e8e6
fabientolsy <2031900@cgmatane.qc.ca>
2023-03-28 16:14:27 -0400

    Ajout de la main droite pour entrer le code du digicode

--- Assets/detectionContact.cs
+++ Assets/detectionContact.cs
@@ -16,7 +16,7 @@ public class detectionContact : MonoBehaviour
     }
     private void OnTriggerEnter(Collider collision)
     {        
-        if (collision.name == "hands:b_l_index3" && Time.time - temps >= .5f)
+        if (collision.name == "hands:b_l_index3" && Time.time - temps >= .5f || collision.name == "hands:b_r_index3" && Time.time - temps >= .5f)
         {
             Debug.LogWarning("Temps depuis le debut: " + temps );
             Debug.Log(collision.name + " est entre en collision avec " + this.gameObject.name);
