aed52c8872ce537dddd3f4534d6fddc88a23e724
Antonin <80816546+ADepoilly@users.noreply.github.com>
2023-04-04 15:59:56 -0400

    maj menu(3)

---
+++ Assets/Respawn.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject gameOverUI;
+
+    //public static bool isDead = false;
+
+    /*void Start()
+    {
+
+    }
+
+    public void gameOver()
+    {
+        Debug.log("game Over");
+        // SceneMamager.LoadScene(SceneManager.GetActiveScene().name);
+
+        gameOverUI.SetActive(true);
+        Time.timeScale = 0;
+        isDead = true;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }*/
+
+    public void respawn()
+    {
+        gameOverUI.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
