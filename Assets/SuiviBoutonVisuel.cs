f4e15cbf235a34704fe4ad0eaa8e74418cccb9a8
Micka09 <mickaelsouloumiac9@gmail.com>
2023-04-06 08:27:10 -0400

    Test Grab avec Main

--- Assets/SuiviBoutonVisuel.cs
+++ Assets/SuiviBoutonVisuel.cs
@@ -3,92 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class SuiviBoutonVisuel : MonoBehaviour
+public class PourVisuelBouton : MonoBehaviour
 {
-    public Transform cibleVisuel;
-    public Vector3 localAxis;
-    private Vector3 offset;
-
-    private Transform pokeAttachTransform;
     private XRBaseInteractable interactable;
-    private Vector3 posInitialLoc;
+    private bool estSuivi;
+    public Vector3 localAxis; 
 
-    public float reinitVitesse = 5;
-    public float angleSuivi;
-
-    private bool estSuivi = false;
-    private bool geler = false;
+    public Transform cibleVisuel;
+    public Vector3 offset;
+    private Transform pokeAttachTransform;
 
     // Start is called before the first frame update
     void Start()
     {
-        posInitialLoc= cibleVisuel.localPosition;
-
         interactable = GetComponent<XRBaseInteractable>();
-        interactable.hoverEntered.AddListener(Suivre);
-        interactable.hoverExited.AddListener(Reinitialiser);
-        interactable.selectEntered.AddListener(Geler);
-        
-    }
-    public void Suivre(BaseInteractionEventArgs survol) 
-    {
-       if(survol.interactorObject is XRPokeInteractor)
-        {
-            XRPokeInteractor interaction = (XRPokeInteractor)survol.interactorObject;
-            estSuivi= true;
-            geler= false;
+        interactable.hoverEntered.AddListener(Follow);
 
-            pokeAttachTransform = interaction.attachTransform;
+    }
+
+    public void Follow(BaseInteractionEventArgs hover)
+    {
+
+        if (hover.interactorObject is XRPokeInteractor)
+        {
+            XRPokeInteractor interactor = (XRPokeInteractor)hover.interactorObject;
+            estSuivi = true;
+            pokeAttachTransform = interactor.attachTransform;
             offset = cibleVisuel.position - pokeAttachTransform.position;
-
-            float pokeAngle = Vector3.Angle(offset, cibleVisuel.TransformDirection(localAxis));
-
-            if(pokeAngle < angleSuivi) 
-            {
-                estSuivi = true;
-                geler = false;
-            }
-        }
-    
-    }
-
-    public void Reinitialiser(BaseInteractionEventArgs survol) 
-    {
-        if(survol.interactorObject is XRPokeInteractor) 
-        {
-            estSuivi= false;
-            geler= false;
-        }
-    }
-
-    public void Geler(BaseInteractionEventArgs survol)
-    {
-        if (survol.interactorObject is XRPokeInteractor)
-        {
-            geler = true;
         }
 
     }
-
-
     // Update is called once per frame
     void Update()
     {
-        if(geler) 
-            return;
-        
-        if(estSuivi) 
+
+        if (estSuivi)
         {
+
             Vector3 localTargertPosition = cibleVisuel.InverseTransformPoint(pokeAttachTransform.position + offset);
-            Vector3 constrainedLocalTargetPosition = Vector3.Project(localTargertPosition, localAxis);
+            Vector3 constrainedLocalTargetPosition = Vector3.Project(localTargertPosition,localAxis);
 
             cibleVisuel.position = cibleVisuel.TransformPoint(constrainedLocalTargetPosition);
-        }
-        else
-        {
-            cibleVisuel.localPosition = Vector3.Lerp(cibleVisuel.localPosition, posInitialLoc, Time.deltaTime * reinitVitesse);
 
         }
-        
     }
 }
