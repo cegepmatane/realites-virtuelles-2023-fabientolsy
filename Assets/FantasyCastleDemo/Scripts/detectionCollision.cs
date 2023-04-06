655abf78671670445cd454c0ce02401017e7d6b9
fabientolsy <2031900@cgmatane.qc.ca>
2023-04-06 08:58:57 -0400

    Le tableau tombe au contact de la main

--- Assets/FantasyCastleDemo/Scripts/detectionCollision.cs
+++ Assets/FantasyCastleDemo/Scripts/detectionCollision.cs
@@ -8,6 +8,10 @@ public class detectionCollision : MonoBehaviour
     [Header("Mur a desactiver")]
     public GameObject mur;
 
+    [Header("RigidBody avec la graviter a activer")]
+    public Rigidbody rb;
+
+
     public void OnTriggerEnter(Collider collision)
     {
         Debug.Log(collision.name + " est entre en collision avec " + this.gameObject.name);
@@ -15,6 +19,9 @@ public class detectionCollision : MonoBehaviour
         {
             Debug.Log("Desactivation du mur");
             mur.SetActive(false);
+
+            rb.useGravity = true;
+            rb.isKinematic = false;
         }
     }
 }
